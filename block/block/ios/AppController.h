180f5e4639d9741338930577d15008a326d2b1af
root <root@ikeda-no-iMac.local>
2014-01-24 17:16:35 +0900

    新規作成

---
+++ block/block/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  blockAppController.h
+//  block
+//
+//  Created by root on 2014/01/24.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ block/block/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  blockAppController.h
+//  block
+//
+//  Created by root on 2014/01/24.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
